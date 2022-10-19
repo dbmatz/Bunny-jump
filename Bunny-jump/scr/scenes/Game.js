5fbd2feed94f15eff0cc8aec5e6c104efd91a2a5
Débora Matzenbacher <108303548+dbmatz@users.noreply.github.com>
2022-10-19 19:52:52 -0400

    Add files via upload

---
+++ Bunny-jump/scr/scenes/Game.js
@@ -0,0 +1,23 @@
+import Phaser from "../lib/phaser.js";
+
+export default class Game extends Phaser.Scene
+{
+    constructor()
+    {
+        super('game');
+    }
+
+    preload()
+    {
+        this.load.image('background', './assets/bg_layer1.png');
+
+        this.load.image('plataform', './assets/ground_grass.png');
+    }
+
+    create()
+    {
+        this.add.image(240, 320, 'background');
+
+        this.add.image(240,320, 'plataform');
+    }
+}
