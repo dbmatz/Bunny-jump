357e02850a3b811e5eff792d96c7c846ce247652
Débora Matzenbacher <108303548+dbmatz@users.noreply.github.com>
2022-10-26 15:04:08 -0400

    Add-collider

--- Bunny-jump/scr/scenes/Game.js
+++ Bunny-jump/scr/scenes/Game.js
@@ -2,6 +2,12 @@ import Phaser from "../lib/phaser.js";
 
 export default class Game extends Phaser.Scene
 {
+    /** @type {Phaser.Physics.Arcade.StaticGroup} */
+    plataforms
+
+    /** @type {Phaser.Physics.Arcade.Sprite} */
+    player
+
     constructor()
     {
         super('game');
@@ -12,12 +18,56 @@ export default class Game extends Phaser.Scene
         this.load.image('background', './assets/bg_layer1.png');
 
         this.load.image('plataform', './assets/ground_grass.png');
+
+        this.load.image('bunny-stand', './assets/bunny1_stand.png');
     }
 
     create()
     {
         this.add.image(240, 320, 'background');
 
-        this.add.image(240,320, 'plataform');
+        this.plataforms = this.physics.add.staticGroup()
+        for(let i = 0 ; i < 5 ; i++){
+            const x = Phaser.Math.Between(80, 400);
+            const y = 150 * i;
+
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            const plataform = this.plataforms.create(x, y, 'plataform');
+            plataform.scale = 0.5;
+
+            /** @type {Phaser.Physics.Arcade.StaticBody} */
+            const body = plataform.body;
+            body.updateFromGameObject();
+        }
+
+        this.player = this.physics.add.sprite(240, 320, 'bunny-stand')
+        .setScale(0.5);
+
+        this.physics.add.collider(this.plataforms, this.player)
+
+        this.player.body.checkCollision.up = false
+        this.player.body.checkCollision.left = false
+        this.player.body.checkCollision.right = false
+
+        this.cameras.main.startFollow(this.player)
+    }
+
+    update(){
+        
+        this.plataforms.children.iterate(child => {
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            const plataform = child
+            const scrollY = this.cameras.main.scrollY
+            if(plataform.y >= scrollY + 700){
+                plataform.y = scrollY - Phaser.Math.Bernstein(50, 100)
+                plataform.body.updateFromGameObject()
+            }
+        })
+
+        const touchingDown = this.player.body.touching.down
+
+        if(touchingDown){
+            this.player.setVelocityY(-300)
+        }
     }
 }
