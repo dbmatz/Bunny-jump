3af019f9e13240074aec0759a4443a4948b196d9
Débora Matzenbacher <108303548+dbmatz@users.noreply.github.com>
2022-10-26 15:17:48 -0400

    Adding-movement

--- Bunny-jump/scr/scenes/Game.js
+++ Bunny-jump/scr/scenes/Game.js
@@ -8,6 +8,9 @@ export default class Game extends Phaser.Scene
     /** @type {Phaser.Physics.Arcade.Sprite} */
     player
 
+    /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+    cursors
+
     constructor()
     {
         super('game');
@@ -20,11 +23,14 @@ export default class Game extends Phaser.Scene
         this.load.image('plataform', './assets/ground_grass.png');
 
         this.load.image('bunny-stand', './assets/bunny1_stand.png');
+
+        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     create()
     {
-        this.add.image(240, 320, 'background');
+        this.add.image(240, 320, 'background')
+            .setScrollFactor(1, 0)
 
         this.plataforms = this.physics.add.staticGroup()
         for(let i = 0 ; i < 5 ; i++){
@@ -52,22 +58,32 @@ export default class Game extends Phaser.Scene
         this.cameras.main.startFollow(this.player)
     }
 
-    update(){
+    update(t, dt){
         
         this.plataforms.children.iterate(child => {
             /** @type {Phaser.Physics.Arcade.Sprite} */
             const plataform = child
+            
             const scrollY = this.cameras.main.scrollY
-            if(plataform.y >= scrollY + 700){
-                plataform.y = scrollY - Phaser.Math.Bernstein(50, 100)
-                plataform.body.updateFromGameObject()
+            if (plataform.y >= scrollY + 700)
+            {
+            plataform.y = scrollY - Phaser.Math.Between(50, 100)
+            plataform.body.updateFromGameObject()
             }
-        })
+            })
 
         const touchingDown = this.player.body.touching.down
 
         if(touchingDown){
             this.player.setVelocityY(-300)
+        }
+
+        if(this.cursors.left.isDown && !touchingDown){
+            this.player.setAccelerationX(-200)
+        }else if(this.cursors.right.isDown && !touchingDown){
+            this.player.setAccelerationX(200)
+        }else{
+            this.player.setVelocityX(0)
         }
     }
 }
